432fd94e8c115c7a248660f0cff39da2d19e2b80
wangsa19 <dewanggawpw@gmail.com>
2025-01-05 16:46:51 +0700

    change static url to dynamic url - be

--- backend/index.js
+++ backend/index.js
@@ -140,7 +140,7 @@ app.post("/image-upload", upload.single("image"), async (req, res) => {
             return res.status(400).json({ error: true, message: "No image uploaded" });
         }
 
-        const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+        const imageUrl = `${process.env.BASE_URL}/uploads/${req.file.filename}`;
         res.status(200).json({ imageUrl });
     } catch (error) {
         res.status(500).json({ error: true, message: error.message });
