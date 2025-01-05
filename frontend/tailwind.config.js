87cf07f8ea7ab6fa1f77a51696d0416f9efa5c48
wangsa19 <dewanggawpw@gmail.com>
2025-01-05 16:14:24 +0700

    change path image fe

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,8 +15,8 @@ export default {
                 secondary: "#EF863E"
             },
             backgroundImage: {
-                'login-bg-img': "url('./src/assets/images/bg-image.png')",
-                'signup-bg-img': "url('./src/assets/images/signup-bg-img.png')",
+                'login-bg-img': "url('./images/bg-image.png')",
+                'signup-bg-img': "url('./images/signup-bg-img.png')",
             }
         },
     },
